3ea3e86228fbf7ea58208afda21b980f8a127a3e
Sinchana T <sinchana7799@gmail.com>
2025-05-12 19:08:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:globalchat/firebase_options.dart';
+
+Future<void> main() async {
+  //initialing the firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        fontFamily: "Poppins",
+      ),
+
+      home: Scaffold(body: Center(child: Text('Firebase'))),
+    );
+  }
+}
